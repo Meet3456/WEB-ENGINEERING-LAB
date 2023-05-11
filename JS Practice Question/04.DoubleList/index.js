c4a1a33a014e23b9326ee9f9097dbb625d6a7c48
Vineet Chotaliya <105866260+Vineetttt@users.noreply.github.com>
2023-05-11 16:19:07 +0530

    JavaScript Practice Questions-Solution

---
+++ JS Practice Question/04.DoubleList/index.js
@@ -0,0 +1,10 @@
+﻿function double(){
+    var input = document.getElementById("inputList").value;
+    var result = document.getElementById("result");
+
+    var numbers = input.split(",").map(Number);
+    for (var i = 0; i < numbers.length; i++) {
+        numbers[i] *= 2;
+    }
+    result.value = "List after doubling the numbers: "+numbers.join(", ");
+}
---
+++ JS Practice Question/10.CheckUppercase/index.js
@@ -0,0 +1,13 @@
+﻿function isFirstCharUppercase(str) {
+    if (!str || !str.charAt(0).match(/[A-Za-z]/)) {
+      console.log("Input string does not start with a character");
+    }
+    else if(str.charAt(0) === str.charAt(0).toUpperCase()){
+        console.log("First Character is Uppercase");
+    }else{
+        console.log("First Character is Lowercase");
+    }
+  }
+  isFirstCharUppercase("Vineet");
+  isFirstCharUppercase("vineet");
+  isFirstCharUppercase("2004Vineet");
