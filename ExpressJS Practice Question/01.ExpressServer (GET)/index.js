81d770846d06cdfc3c7ca1d8cb6c3b240b0918c3
Vineet Chotaliya <105866260+Vineetttt@users.noreply.github.com>
2023-05-11 17:52:26 +0530

    ExpressJS Practice Question-Solution

---
+++ ExpressJS Practice Question/01.ExpressServer (GET)/index.js
@@ -0,0 +1,19 @@
+﻿const express = require('express');
+const app = express();
+
+// Route for the home page
+app.get('/', (req, res) => {
+  res.send('Welcome to the home page!');
+});
+
+// Route for books information
+app.get('/books', (req, res) => {
+  const books = ['Think Like a Monk', 'Rich Dad, Poor Dad', 'The power of your Subconcious Mind'];
+  res.send(`Here are some popular books : ${books.join(', ')}`);
+});
+
+// Start the server
+const port = 4000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
---
+++ ExpressJS Practice Question/02.ExpressServer (GET and POST)/index.js
@@ -0,0 +1,51 @@
+﻿const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 4000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// GET request handler for route '/'
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <title>Sample Form</title>
+      </head>
+      <body>
+        <h1>Sample Form</h1>
+        <form action="/" method="post">
+          <label for="name">Name:</label>
+          <input type="text" id="name" name="name"><br><br>
+          <label for="email">Email:</label>
+          <input type="email" id="email" name="email"><br><br>
+          <button type="submit">Submit</button>
+        </form>
+      </body>
+    </html>
+  `);
+});
+
+// POST request handler for route '/'
+app.post('/', (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  res.send(`
+    <html>
+      <head>
+        <title>Form Data</title>
+      </head>
+      <body>
+        <h1>Form Data</h1>
+        <p>Name: ${name}</p>
+        <p>Email: ${email}</p>
+      </body>
+    </html>
+  `);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
