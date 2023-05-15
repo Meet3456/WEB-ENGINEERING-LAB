8b7c8c319d5d447c7cf61a21897a79700e8e3af5
Vineet Chotaliya <105866260+Vineetttt@users.noreply.github.com>
2023-05-16 00:45:49 +0530

    ReactJS Practice Questions

---
+++ ReactJS Practice Questions/react-practice/src/Q1.js
@@ -0,0 +1,25 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+
+// Single Element
+const message = <h1>Hello World!</h1>;
+
+// Component with Multiple Elements
+function Question1() {
+  return (
+    <div>
+      <h1>Welcome to My App</h1>
+      <p>This is a paragraph</p>
+      <ul>
+        <li>List Item 1</li>
+        <li>List Item 2</li>
+        <li>List Item 3</li>
+      </ul>
+    </div>
+  );
+}
+
+ReactDOM.render(message, document.getElementById("root")); // Render single element
+ReactDOM.render(<Question1 />, document.getElementById("root")); // Render component with multiple elements
+
+export default Question1;
---
+++ ReactJS Practice Questions/react-practice/src/Question5.js
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+import Card from './Card';
+
+function Question5() {
+  return (
+    <>
+    <h1>Implementing Props</h1>
+    <Card title="Think Like A Monk" author="Jay Shetyy" description="Lorem epsum lorefegre htgre ertyhg xcvbni frtyuejfnbvc rtyujnbce dfghjnbvdgeh " />
+    <Card title="Rich Dad Poor Dad" author="Robert Kiyosak" description="Lorem epsum lorefegre htgre ertyhg xcvbni frtyuejfnbvc rtyujnbce dfghjnbvdgeh " />
+    <Card title="The Intelligent Investor" author="Benjamin Graham" description="Lorem epsum lorefegre htgre ertyhg xcvbni frtyuejfnbvc rtyujnbce dfghjnbvdgeh " />
+    
+    </>
+  )
+}
+
+export default Question5
